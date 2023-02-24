07bd890aa576668787a435483fed9890810f56fe
rezaan6 <rezaan6@gmail.com>
2023-02-24 09:43:35 +0530

    Improvement to Image generator

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     const fetchPosts = async () => {
       setLoading(true);
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
+        const response = await fetch("https://open-ai-dalle-2-image-generator.onrender.com/api/v1/post", {
           method: "GET",
           headear: {
             "Content-Type": "application/json",
