531a98affd109b20e965696843db38813509f92c
rezaan6 <rezaan6@gmail.com>
2023-03-11 17:51:14 +0530

    update postRoutes.js

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -18,8 +18,12 @@ cloudinary.config({
 router.route("/").get(async (req, res) => {
   try {
     const posts = await Post.find({});
-    console.log(posts);
-    res.status(200).json({ success: true, data: posts });
+    const updatedPosts = posts.map((post) => {
+      const updatedPost = { ...post._doc };
+      updatedPost.photo = cloudinary.url(post.photo);
+      return updatedPost;
+    });
+    res.status(200).json({ success: true, data: updatedPosts });
   } catch (error) {
     res.status(200).json({ success: false, message: error });
   }
